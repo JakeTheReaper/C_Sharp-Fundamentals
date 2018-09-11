0292c992e93acdcc7be6d0a599570d6ec4ffb0d9
Jake Zerafa <jake.zerafa86@gmail.com>
2018-09-11 12:52:41 +1000

    Updated Code

---
+++ C_Sharp-Fundamentals/C_Sharp-Fundamentals/Rectangle.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Fundamentals
+{
+    class Rectangle
+    {
+        static void Main(string[] args)
+        {
+            //member variables
+            double length = 5.0;
+            double width = 10.0;
+
+            Console.WriteLine(length * width);
+            Console.ReadKey();
+        }
+
+    }
+
+}
